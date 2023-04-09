52df09c304bdd21e63d0ac87413bb0d8a236a4ad
Prashant Singh <psprashantsingh91@gmail.com>
2023-04-09 22:59:42 +0530

    first prg

---
+++ FirstPrg.java
@@ -0,0 +1,14 @@
+// package DS-JAVA;
+
+import java.util.Scanner;
+
+class FirstPrg {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        Scanner sc = new Scanner(System.in); // System.in is a standard input stream.
+        System.out.print("Enter first number- ");
+        int a = sc.nextInt();
+        System.out.println(a);
+
+    }
+}
