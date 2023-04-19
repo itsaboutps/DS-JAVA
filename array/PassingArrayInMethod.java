2a44fe3d448259b3ec231e3569abf21a7541c5a1
Prashant Singh <singh.p@novelvox.com>
2023-04-19 14:29:05 -0700

    Data Structures in Java

---
+++ array/PassingArrayInMethod.java
@@ -0,0 +1,26 @@
+package array;
+
+// Java program to demonstrate
+// passing of array to method
+
+public class PassingArrayInMethod {
+	// Driver method
+	public static void main(String args[])
+	{
+		int arr[] = { 3, 1, 2, 5, 4 };
+
+		// passing array to method m1
+		sum(arr);
+	}
+
+	public static void sum(int[] arr)
+	{
+		// getting sum of array values
+		int sum = 0;
+
+		for (int i = 0; i < arr.length; i++)
+			sum += arr[i];
+
+		System.out.println("sum of array values : " + sum);
+	}
+}
